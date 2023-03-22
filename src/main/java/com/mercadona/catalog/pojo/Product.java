8a513032d2674dd72b4ea7a546afd0117c6c3ae3
FD-FuRy <83590934+FD-FuRy@users.noreply.github.com>
2023-03-22 14:42:25 +0100

    Création de la classe Product
    Création de la classe Product (avec ses attributs de base, sans aucune méthode)

---
+++ src/main/java/com/mercadona/catalog/pojo/Product.java
@@ -0,0 +1,12 @@
+package com.mercadona.catalog.pojo;
+
+public class Product {
+    private Long productId;
+    private String productName;
+    private double productPrice;
+    private String imageDirectory;
+
+    private ProductCategory productCategory;
+    private ProductPromotion productPromotion;
+
+}
