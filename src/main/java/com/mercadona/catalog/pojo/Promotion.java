1f60f236e2de6ffb3e2fb24e8a1c4d64793acaf6
FD-FuRy <83590934+FD-FuRy@users.noreply.github.com>
2023-03-27 11:10:59 +0200

    Création des méthodes (constructeurs, getters/setters) par défaut de Promotion
    Création des méthodes par défaut de la classe Promotion:
    - Constructeurs
    - Getters/ Setters

--- src/main/java/com/mercadona/catalog/pojo/Promotion.java
+++ src/main/java/com/mercadona/catalog/pojo/Promotion.java
@@ -8,4 +8,40 @@ public class Promotion {
     private Date promotionEndDate;
     private int discountPercentage;
 
+    public Promotion() {
+    }
+    public Promotion(Long promotionId, Date promotionStartDate, Date promotionEndDate, int discountPercentage) {
+        this.promotionId = promotionId;
+        this.promotionStartDate = promotionStartDate;
+        this.promotionEndDate = promotionEndDate;
+        this.discountPercentage = discountPercentage;
+    }
+
+    public Long getPromotionId() {
+        return promotionId;
+    }
+    public void setPromotionId(Long promotionId) {
+        this.promotionId = promotionId;
+    }
+
+    public Date getPromotionStartDate() {
+        return promotionStartDate;
+    }
+    public void setPromotionStartDate(Date promotionStartDate) {
+        this.promotionStartDate = promotionStartDate;
+    }
+
+    public Date getPromotionEndDate() {
+        return promotionEndDate;
+    }
+    public void setPromotionEndDate(Date promotionEndDate) {
+        this.promotionEndDate = promotionEndDate;
+    }
+
+    public int getDiscountPercentage() {
+        return discountPercentage;
+    }
+    public void setDiscountPercentage(int discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
 }
