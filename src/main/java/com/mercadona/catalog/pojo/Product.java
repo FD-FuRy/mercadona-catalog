fe949396acbff68afb523dab6715aa5af86c7dbb
FD-FuRy <83590934+FD-FuRy@users.noreply.github.com>
2023-03-27 11:22:20 +0200

    Création des méthodes (constructeurs, getters/setters) par défaut de Product
    Création des méthodes par défaut de la classe Product:
    - Constructeurs
    - Getters/ Setters

--- src/main/java/com/mercadona/catalog/pojo/Product.java
+++ src/main/java/com/mercadona/catalog/pojo/Product.java
@@ -9,4 +9,56 @@ public class Product {
     private ProductCategory productCategory;
     private Promotion promotion;
 
+    public Product() {
+    }
+    public Product(Long productId, String productName, double productPrice, String imageDirectory, ProductCategory productCategory) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.imageDirectory = imageDirectory;
+        this.productCategory = productCategory;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getImageDirectory() {
+        return imageDirectory;
+    }
+    public void setImageDirectory(String imageDirectory) {
+        this.imageDirectory = imageDirectory;
+    }
+
+    public ProductCategory getProductCategory() {
+        return productCategory;
+    }
+    public void setProductCategory(ProductCategory productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public Promotion getPromotion() {
+        return promotion;
+    }
+    public void setPromotion(Promotion promotion) {
+        this.promotion = promotion;
+    }
+
 }
