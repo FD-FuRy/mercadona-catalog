5aafd808613af145bce69884a415b801a2f0290f
FD-FuRy <83590934+FD-FuRy@users.noreply.github.com>
2023-03-27 16:12:44 +0200

    Ajout de commentaires à l' ApiRegistration.java
    Ajout de commentaires décrivant le service et les variables qui y sont énumérées

--- src/main/java/com/mercadona/catalog/api/ApiRegistration.java
+++ src/main/java/com/mercadona/catalog/api/ApiRegistration.java
@@ -1,5 +1,8 @@
 package com.mercadona.catalog.api;
 
+//  Création des méthodes (énumération de variables à appeler ici) pour structurer l'application et faciliter la connexion api.
+//  (appeler ces variables dans les web services évite aussi d'écrire en dur dans les appels).
+//  Si plus tard, évolution de l'appli il y a, il suffira de modifier les variables seulement ici (et pas dans tous les sercices appelants).
 public interface ApiRegistration {
 
     String REST_PREFIX = "/api/";
