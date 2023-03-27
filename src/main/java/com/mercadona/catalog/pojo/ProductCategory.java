68ed39c031e36cf714b58a8b5d571f7a3cd9a77b
FD-FuRy <83590934+FD-FuRy@users.noreply.github.com>
2023-03-27 11:02:55 +0200

    Création des méthodes (constructeurs, getters/setters) par défaut de ProductCategory
    Création des méthodes par défaut de la classe ProductCategory:
    - Constructeur
    - Getters/ Setters

--- src/main/java/com/mercadona/catalog/pojo/ProductCategory.java
+++ src/main/java/com/mercadona/catalog/pojo/ProductCategory.java
@@ -4,4 +4,22 @@ public class ProductCategory {
     private Long categoryId;
     private String categoryName;
 
+    public ProductCategory(Long categoryId, String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
