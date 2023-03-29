78d9885f52892c86cad4985aa24eca78d6bd194c
FD-FuRy <83590934+FD-FuRy@users.noreply.github.com>
2023-03-29 18:19:21 +0200

    Modification du controleur ProductWS pour y appeler les méthodes de ProductService
    Modification du contrôleur ProductWS:
    - On appele et utilise les méthodes de ProductService

--- src/main/java/com/mercadona/catalog/api/ProductWS.java
+++ src/main/java/com/mercadona/catalog/api/ProductWS.java
@@ -1,6 +1,9 @@
 package com.mercadona.catalog.api;
 
 import com.mercadona.catalog.pojo.Product;
+import com.mercadona.catalog.pojo.ProductCategory;
+import com.mercadona.catalog.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,28 +13,38 @@ import java.util.List;
 @RequestMapping( ApiRegistration.REST_PREFIX + ApiRegistration.REST_CATALOGUE )
 public class ProductWS {
 
+    @Autowired    //appel des méthodes ProductService avec autoconfiguration Spring
+    private ProductService productService;
+
     // Map de la méthode Get des produits
     @GetMapping
     public List<Product> getAllProducts() {
-        return null;   // implémentation de la méthode Get des produits ici
+        return productService.getAllProducts();
+    }
+
+    // Map de la méthode Get By (catégorie) des produits
+    @GetMapping("{productCategory}")
+    public List<Product> getAllProductsByCategory(@PathVariable(name = "productCategory") ProductCategory productCategory) {
+        return productService.getAllProductsByCategory(productCategory);
     }
 
     // Map de la méthode Post des produits
     @PostMapping
     public void createProduct(@RequestBody Product product) {
-        // implémentation de la méthode Post ici
+        productService.createProduct(product);
     }
 
     // Map de la méthode Put des produits
-    @PutMapping("{name}")
-    public void updateProduct(@PathVariable(name= "name") String nom) {
-        // implémentation de la méthode Put ici
+    @PutMapping("{productId}")
+    public void updateProduct(@PathVariable(name= "productId") Long productId,
+                              @RequestBody Product product) {
+        productService.updateProduct(productId, product);
     }
 
     // Map de la méthode Delete des produits
-    @DeleteMapping ("{name}")
-    public void deleteProduct(@PathVariable(name= "name") String nom) {
-        // implémentation de la méthode Delete ici
+    @DeleteMapping ("{productId}")
+    public void deleteProduct(@PathVariable(name= "productId") Long productId) {
+        productService.deleteProduct(productId);
     }
 
 }
