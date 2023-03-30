873607c059727c6455cbb44b6340f973ad732fdb
FD-FuRy <83590934+FD-FuRy@users.noreply.github.com>
2023-03-30 12:16:16 +0200

    Ajout d'annotations dans ProductWS pour affichage des méthodes dans l'UI Swagger
    Ajout d'annotations à mes Map Request dans ProductWS afin d'afficher les noms des méthodes (et une description) dans l'UI html de Swagger.
    
    Les noms des méthodes s'affichaient automatiquement via SpringFox mais pas via SpringDoc Open API dans l'UI de SWAGGER. Utilisation d'annotations @operation (summary) pour afficher du texte dans l'UI.

--- src/main/java/com/mercadona/catalog/api/ProductWS.java
+++ src/main/java/com/mercadona/catalog/api/ProductWS.java
@@ -3,6 +3,7 @@ package com.mercadona.catalog.api;
 import com.mercadona.catalog.pojo.Product;
 import com.mercadona.catalog.pojo.ProductCategory;
 import com.mercadona.catalog.services.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,24 +18,28 @@ public class ProductWS {
     private ProductService productService;
 
     // Map de la méthode Get des produits
+    @Operation(operationId = "getAllProducts", summary = "getAllProducts  ( Afficher tous les produits )")
     @GetMapping
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
 
     // Map de la méthode Get By (catégorie) des produits
+    @Operation(operationId = "getAllProductsByCategory", summary = "getAllProductsByCategory  ( Afficher tous les produits d'une catégorie )")
     @GetMapping("{productCategory}")
     public List<Product> getAllProductsByCategory(@PathVariable(name = "productCategory") ProductCategory productCategory) {
         return productService.getAllProductsByCategory(productCategory);
     }
 
     // Map de la méthode Post des produits
+    @Operation(operationId = "createProduct", summary = "createProduct  ( Créer un nouveau produit )")
     @PostMapping
     public void createProduct(@RequestBody Product product) {
         productService.createProduct(product);
     }
 
     // Map de la méthode Put des produits
+    @Operation(operationId = "updateProduct", summary = "updateProduct  ( Modifier un produit existant )")
     @PutMapping("{productId}")
     public void updateProduct(@PathVariable(name= "productId") Long productId,
                               @RequestBody Product product) {
@@ -42,6 +47,7 @@ public class ProductWS {
     }
 
     // Map de la méthode Delete des produits
+    @Operation(operationId = "deleteProduct", summary = "deleteProduct  ( Supprimer un produit )")
     @DeleteMapping ("{productId}")
     public void deleteProduct(@PathVariable(name= "productId") Long productId) {
         productService.deleteProduct(productId);
