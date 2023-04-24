305587bd037cdf49ea7237fbabbd1d93539af155
FD-FuRy <83590934+FD-FuRy@users.noreply.github.com>
2023-04-24 12:06:18 +0200

    Mapping du ModelAndView "createProduct" dans le contrôleur de vue Admin
    - Mapping du ModelAndView "createProduct" dans ModelAdminControleur pour la gestion de la vue de la page de création de produits.

--- src/main/java/com/mercadona/catalog/api/ModelAdminControler.java
+++ src/main/java/com/mercadona/catalog/api/ModelAdminControler.java
@@ -1,5 +1,6 @@
 package com.mercadona.catalog.api;
 
+import com.mercadona.catalog.pojo.Product;
 import com.mercadona.catalog.services.ProductCategoryService;
 import com.mercadona.catalog.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +13,28 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/admin/")
 public class ModelAdminControler {
 
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private ProductCategoryService productCategoryService;
+
     // MAP du ModelAndView de la page d'accueil du panneau administrateur:
     @GetMapping
     public ModelAndView getAdminPage() {
         ModelAndView getAdminLandingPage = new ModelAndView();
         getAdminLandingPage.setViewName("pages/admin.html");
         return getAdminLandingPage;
+    }
+
+    //MAP du ModelAndView de la page Admin de création d'un nouveau produit:
+    @GetMapping ("/product/create")
+    public ModelAndView createProduct() {
+        ModelAndView getNewProductData = new ModelAndView();
+        getNewProductData.setViewName("/admin/create-product.html");
+        Product product = new Product();
+        getNewProductData.addObject("product1", product);
+        getNewProductData.addObject("allCategoriesList", productCategoryService.getAllProductsCategory());
+        return getNewProductData;
     }
 
 }
