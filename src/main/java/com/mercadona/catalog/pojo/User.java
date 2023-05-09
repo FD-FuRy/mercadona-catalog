6ea1b419a181892089f251bdf6ae6d369d133ee5
FD-FuRy <83590934+FD-FuRy@users.noreply.github.com>
2023-05-09 15:03:21 +0200

    Création des classes User et UserRole
    Création des classes User et UserRole pour la future authentification

---
+++ src/main/java/com/mercadona/catalog/pojo/User.java
@@ -0,0 +1,77 @@
+package com.mercadona.catalog.pojo;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+public class User {
+    //Id clé primaire de la base "Users" (auto-générée par séquence):
+    @Id
+    @SequenceGenerator(name="user_sequence",
+    sequenceName = "user_sequence",
+    allocationSize = 113)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+    generator = "user_sequence")
+    @Column(name = "userId", updatable = false)
+    private Long userId;
+
+    @Column(name="userName")
+    private String userName;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name="userRoles",
+            joinColumns = {@JoinColumn(name="userId", referencedColumnName = "userId")},
+            inverseJoinColumns = {@JoinColumn(name = "roleId", referencedColumnName = "roleId")})
+    private List<UserRole> roles =  new ArrayList<>();
+
+    // constructeurs de la classe Utilisateurs:
+    public User() {
+        //Constructeur vide requis par hibernate
+    }
+
+    public User(String userName, String email, String password, List<UserRole> roles) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public User(Long userId, String userName, String email, String password, List<UserRole> roles) {
+        this.userId = userId;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    //Getters/Setters du nombre identifiant de l'utilisateur:
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+
+    //Getters/Setters du nom de l'utilisateur:
+    public String getUserName() { return userName; }
+    public void setUserName(String userName) { this.userName = userName; }
+
+    //Getters/Setters de l'email utilisateur:
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    //Getters/Setters du mot de passe de l'utilisateur:
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    //Getters/Setters du/des roles/grades de l'utilisateur:
+    public List<UserRole> getRoles() { return roles; }
+    public void setRoles(List<UserRole> roles) { this.roles = roles; }
+
+}
---
+++ src/main/java/com/mercadona/catalog/pojo/UserRole.java
@@ -0,0 +1,55 @@
+package com.mercadona.catalog.pojo;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="Roles")
+public class UserRole {
+    //Id clé primaire de la base "UserRoles" (auto-générée par séquence):
+    @Id
+    @SequenceGenerator(name="role_sequence",
+            sequenceName = "role_sequence",
+            allocationSize = 113)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "role_sequence")
+    @Column(name = "roleId", updatable = false)
+    private Long roleId;
+
+    @Column(name="roleName")
+    private String roleName;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users = new ArrayList<>();
+
+    // constructeurs de la classe Utilisateurs:
+    public UserRole() {
+        //Constructeur vide requis par hibernate
+    }
+
+    public UserRole(String roleName, List<User> users) {
+        this.roleName = roleName;
+        this.users = users;
+    }
+
+    public UserRole(Long roleId, String roleName, List<User> users) {
+        this.roleId = roleId;
+        this.roleName = roleName;
+        this.users = users;
+    }
+
+    //Getters/Setters du nombre identifiant du Role:
+    public Long getRoleId() { return roleId; }
+    public void setRoleId(Long roleId) { this.roleId = roleId; }
+
+    //Getters/Setters du nom du Role:
+    public String getRoleName() { return roleName; }
+    public void setRoleName(String roleName) { this.roleName = roleName; }
+
+    //Getters/Setters du/des utilisateurs ayant ce Role:
+    public List<User> getUsers() { return users; }
+    public void setUsers(List<User> users) { this.users = users; }
+
+}
