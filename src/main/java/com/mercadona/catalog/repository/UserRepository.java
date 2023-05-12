3938fd43a712697ea76e71f25cb6cb359fcf302d
FD-FuRy <83590934+FD-FuRy@users.noreply.github.com>
2023-05-12 16:14:18 +0200

    Fix de UserRepository  (ajout d'une méthode utilisée par CustomUserDetailsService
    Update de UserRepository:   Ajout de méthode findFistByUserName()  en plus de la méthode findByUserName(). Cette nouvelle méthode est utilisée par le CustomUserDetailsService

--- src/main/java/com/mercadona/catalog/repository/UserRepository.java
+++ src/main/java/com/mercadona/catalog/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.mercadona.catalog.repository;
 
-import com.mercadona.catalog.pojo.User;
+import com.mercadona.catalog.pojo.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-    Optional<User> findByUserName(String userName);
+public interface UserRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByEmail(String email);
+    Optional<Users> findByUserName(String userName);
+
+    Users findFirstByUserName(String username);
 }
