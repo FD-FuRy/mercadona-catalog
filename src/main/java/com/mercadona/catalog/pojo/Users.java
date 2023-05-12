dce85355b52468d1c6151aa471d7002da4f6912f
FD-FuRy <83590934+FD-FuRy@users.noreply.github.com>
2023-05-12 16:10:29 +0200

    Fix de la classe User (ajout d'un FetchType dans la relation ManyTo Many
    Fix de la classe User:  ajout d'un FetchType dans la relation ManyTo Many entre User et UserRoles (je n'arrivais pas à m'authentifier car il n'arrivait pas à initialiser et récupérer dans la table la collection "role" à la connexion)

--- src/main/java/com/mercadona/catalog/pojo/Users.java
+++ src/main/java/com/mercadona/catalog/pojo/Users.java
@@ -27,7 +27,7 @@ public class Users {
     @Column(name="password")
     private String password;
 
-    @ManyToMany(cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name="userRoles",
             joinColumns = {@JoinColumn(name="userId", referencedColumnName = "userId")},
             inverseJoinColumns = {@JoinColumn(name = "roleId", referencedColumnName = "roleId")})
