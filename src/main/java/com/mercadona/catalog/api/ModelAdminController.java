5131810791ea963d9a1da0d04a97c71d45314598
FD-FuRy <83590934+FD-FuRy@users.noreply.github.com>
2023-05-22 20:00:50 +0200

    another path change text

--- src/main/java/com/mercadona/catalog/api/ModelAdminController.java
+++ src/main/java/com/mercadona/catalog/api/ModelAdminController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/admin/")
+@RequestMapping("/admin-pannel/")
 public class ModelAdminController {
 
     @Autowired
