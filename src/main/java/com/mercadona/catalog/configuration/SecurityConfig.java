d3b312e060e63db7bd3f848f68aa75c00fa17e41
FD-FuRy <83590934+FD-FuRy@users.noreply.github.com>
2023-05-22 20:14:56 +0200

    always change path test

--- src/main/java/com/mercadona/catalog/configuration/SecurityConfig.java
+++ src/main/java/com/mercadona/catalog/configuration/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
                 .formLogin(
                         form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/admin/")
+                        .defaultSuccessUrl("/?connexion_success")
                         .loginProcessingUrl("/login")
                         .failureUrl("/login?error")
                         .permitAll()
